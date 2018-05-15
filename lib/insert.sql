a005d304d02a8012eb7fa08f0545399cecf0a40a
Mehwish Aziz <mehwisha@msn.com>
2018-05-15 20:32:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears(id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 5, "M", "dark brown", "Cool", 1);
-insert into bears(id, name, age, gender, color, temperament, alive) values (2, "Rowdy", 2, "M", "black", "goofy", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values (2, "Rowdy", 2, "M", "dark brown", "goofy", 1);
 insert into bears(id, name, age, gender, color, temperament, alive) values(3, "Tabitha", 8, "F", "White", "goofy", 1);
 insert into bears(id, name, age, gender, color, temperament, alive) values(4, "Sergeant Brown", 10, "M", "dark brown", "Cool", 1);
 insert into bears(id, name, age, gender, color, temperament, alive) values(5, "Melissa", 15, "F", "light brown", "Hot", 0);
