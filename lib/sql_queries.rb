97518ec4c1b6f0c456005e72f0237b35f9ddd8b0
Mehwish Aziz <mehwisha@msn.com>
2018-05-15 20:02:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "select * from bears where gender = 'F'"
+  "select * from bears where gender = 'F' "
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "select name, age, alive from bears where alive = "T" order by age asc "
+  "select name, age, alive from bears where alive = 1 order by age asc "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "select distinct(color) from bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "select * from bears where temperament = "goofy""
+  "select * from bears where temperament = 'goofy'
 end
 
 def selects_bear_that_killed_Tim
-  "select * from bears where name = "NULL""
+  "select * from bears where name = "NULL" "
 end
