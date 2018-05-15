dee0e39bb732f586fa4ab4cc0f7259e4735eab84
Mehwish Aziz <mehwisha@msn.com>
2018-05-15 20:00:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 5, "M", "Brown", "Cool", "T");
-insert into bears(id, name, age, gender, color, temperament, alive) values (2, "Rowdy", 2, "M", "Black", "goofy", "T");
-insert into bears(id, name, age, gender, color, temperament, alive) values(3, "Tabitha", 8, "F", "White", "goofy", "T");
-insert into bears(id, name, age, gender, color, temperament, alive) values(4, "Sergeant Brown", 10, "M", "Brown", "Cool", "T");
-insert into bears(id, name, age, gender, color, temperament, alive) values(5, "Melissa", 15, "F", "Brown", "Hot", "F");
-insert into bears(id, name, age, gender, color, temperament, alive) values(6, "Grinch", 15, "M", "Brown", "Hot", "T");
-insert into bears(id, name, age, gender, color, temperament, alive) values(7, "Wendy", 5, "F", "Black", "Cool", "T");
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 5, "M", "Brown", "Cool", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values (2, "Rowdy", 2, "M", "Black", "goofy", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values(3, "Tabitha", 8, "F", "White", "goofy", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values(4, "Sergeant Brown", 10, "M", "Brown", "Cool", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values(5, "Melissa", 15, "F", "Brown", "Hot", 0);
+insert into bears(id, name, age, gender, color, temperament, alive) values(6, "Grinch", 15, "M", "Brown", "Hot", 1);
+insert into bears(id, name, age, gender, color, temperament, alive) values(7, "Wendy", 5, "F", "Black", "Cool", 0);
 insert into bears(id, name, age, gender, color, temperament, alive) values(8, NULL, NULL, NULL, NULL, NULL, NULL);
