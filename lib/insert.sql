2ef587c7be0e61e17f73029c66bb03934349db2b
Mehwish Aziz <mehwisha@msn.com>
2018-05-15 19:08:09 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears(1, "Mr. Chocolate", 5, "M", "Brown", "Cool", T);
+  insert into bears(2, "Rowdy", 2, "M", "Black", "Cool", T);
+    insert into bears(3, "Tabitha", 8, "F", "White", "Cool", T);
+      insert into bears(4, "Sergeant Brown", 10, "M", "Brown", "Cool", T);
+        insert into bears(5, "Melissa", 15, "F", "Brown", "Hot", F);
+          insert into bears(6, "Grinch", 15, "M", "Brown", "Hot", T);
+            insert into bears(7, "Wendy", 5, "F", "Black", "Cool", Y);
+              insert into bears(8, NULL, NULL, NULL, NULL, NULL, NULL);
