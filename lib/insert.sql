42e93e1cfeb1b4702a7e267fed1c64e4cc55309f
Mehwish Aziz <mehwisha@msn.com>
2018-05-15 19:16:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears(id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 5, "M", "Brown", "Cool", "T)"";
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 5, "M", "Brown", "Cool", "T);
